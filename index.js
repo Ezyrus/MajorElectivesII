24bdf440785134171913f4cc64d12d6bad6e118f
GoodestBoi! <83266402+Ezyrus@users.noreply.github.com>
2023-01-26 19:56:37 +0800

    config

---
+++ index.js
@@ -0,0 +1,16 @@
+const age = 17
+
+age >=18 ? true : false
+
+console.log(age)
+
+
+
+
+
+
+// var: When declared, it is considered as global-variable.
+// let: Only accessible inside of the block of code.
+// Ternary (?): 
+// Spread (...):
+// var total = (1==2) ? console.log("True"):console.log("False")
