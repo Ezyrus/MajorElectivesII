b93f405a03ae065264ba635db913d602a58cca74
GoodestBoi! <83266402+Ezyrus@users.noreply.github.com>
2023-01-27 11:17:13 +0800

    Revert "config"
    This reverts commit 47083ad30ec1a725cc5ee4f95a83c31490d4c772.

---
+++ index.js
@@ -0,0 +1,10 @@
+var totalAdd = (x, y) => x + y;
+
+console.log(totalAdd(1,3));
+
+// var: When declared, it is considered as global-variable.
+// let: Only accessible inside of the block of code.
+// Ternary (?): 
+// Spread (...):
+// var legalAge = age >=18 ? true : false
+// var totalAdd = (x, y) => x + y;
