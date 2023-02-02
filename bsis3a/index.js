1af915a99651222431a661b13be95886d60150b7
GoodestBoi! <83266402+Ezyrus@users.noreply.github.com>
2023-02-02 20:45:16 +0800

    config

---
+++ bsis3a/index.js
@@ -0,0 +1,51 @@
+// const col01row01 = document.getElementById("1");
+// const col01row02 = document.getElementById("2");
+// const col01row03 = document.getElementById("3");
+
+// const col02row01 = document.getElementById("4");
+// const col02row02 = document.getElementById("5");
+// const col02row03 = document.getElementById("6");
+
+// const col03row01 = document.getElementById("7");
+// const col03row02 = document.getElementById("8");
+// const col03row03 = document.getElementById("9");
+
+// var clickable = document.getElementsByClassName("mark");
+// console.log(clickable);
+
+
+ //   var col01row01 = document.getElementById("1");
+    //   var col01row02 = document.getElementById("2");
+    //   var col01row03 = document.getElementById("3");
+
+    //   var col02row01 = document.getElementById("4");
+    //   var col02row02 = document.getElementById("5");
+    //   var col02row03 = document.getElementById("6");
+
+    //   var col03row01 = document.getElementById("7");
+    //   var col03row02 = document.getElementById("8");
+    //   var col03row03 = document.getElementById("9");
+
+
+
+
+
+
+
+
+
+// var btns = document.getElementsByClassName("btn");
+
+//       for (var btn of btns) {
+//         btn.onclick = function () {
+//           window.alert("hello");
+//         };
+//       }
+      
+// var: When declared, it is considered as global-variable.
+// let: Only accessible inside of the block of code.
+// Ternary (?): 
+// Spread (...):
+// Arrow Function (=>):
+// var legalAge = age >=18 ? true : false (shorthand if statement)
+// var totalAdd = (x, y) => x + y;                (shorthand function)
