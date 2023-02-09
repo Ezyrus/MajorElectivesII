2c78d4b62b0b8181f55e5ecc3c286849ca86e901
GoodestBoi! <83266402+Ezyrus@users.noreply.github.com>
2023-02-09 16:54:09 +0800

    Before Class | 2/9/2023

---
+++ Review.js
@@ -0,0 +1,16 @@
+
+// var btns = document.getElementsByClassName("btn");
+
+//       for (var btn of btns) {
+//         btn.onclick = function () {
+//           window.alert("hello");
+//         };
+//       }
+      
+// var: When declared, it is considered as global-variable.
+// let: Only accessible inside of the block of code.
+// Ternary (?): 
+// Spread (...):
+// Arrow Function (=>):
+// var legalAge = age >=18 ? true : false (shorthand if statement)
+// var totalAdd = (x, y) => x + y;                (shorthand function)
---
+++ activity/index.js
@@ -0,0 +1,11 @@
+// var col01row01 = document.getElementById("1");
+// var col01row02 = document.getElementById("2");
+// var col01row03 = document.getElementById("3");
+
+// var col02row01 = document.getElementById("4");
+// var col02row02 = document.getElementById("5");
+// var col02row03 = document.getElementById("6");
+
+// var col03row01 = document.getElementById("7");
+// var col03row02 = document.getElementById("8");
+// var col03row03 = document.getElementById("9");
