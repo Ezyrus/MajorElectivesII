df9a8c312cb6465f89e46fb25fc278e79c7b1efe
GoodestBoi! <83266402+Ezyrus@users.noreply.github.com>
2023-03-02 16:28:12 +0800

    config

--- Review.js
+++ Review.js
@@ -12,7 +12,8 @@
       
 // var: When declared, it is considered as global-variable.
 // let: Only accessible inside of the block of code.
-// const::
+// const: One time declaration
+
 // Lambda expression: =>
 // var legalAge = age >=18 ? true : false (shorthand if statement)
 // var totalAdd = (x, y) => x + y;                (shorthand function)
