f68946377f8fb0d565fbc8cacd691f95011cd1b7
GoodestBoi! <83266402+Ezyrus@users.noreply.github.com>
2023-03-12 19:43:31 +0800

    config

--- Review.js
+++ Review.js
@@ -1,5 +1,3 @@
-
-
 // var btns = document.getElementsByClassName("btn");
 // var btns = document.getElementsById("btn");
 // var btns = document.getElementsByTagName("btn");
@@ -11,15 +9,23 @@
 //       }
       
 // var: When declared, it is considered as global-variable.
-// let: Only accessible inside of the block of code.
+// var: Only accessible inside of the block of code.
 // const: One time declaration
 
 // Lambda expression: =>
 // var legalAge = age >=18 ? true : false (shorthand if statement)
 // var totalAdd = (x, y) => x + y;                (shorthand function)
 // Operator: Arithmetic (+-*/)
-//                    Assignment (=,/=,x=,+=,-=)
-//                    Comparison (>,<,>=,<=,==)
-//                    Logical (||, &&, !=)
-// Ternary: ? 
+//                 Assignment (=,/=,x=,+=,-=)
+//                 Comparison (>,<,>=,<=,==)
+//                 Logical (||, &&, !=)
+//                 Ternary: ? 
 // Spread: ...
+
+for (var i = 1; i <= 5; i++) {
+    var r = "";
+    for (var j = 5; j >= i; j--) {
+      r += "*";
+    }
+    console.log(r);
+  }
